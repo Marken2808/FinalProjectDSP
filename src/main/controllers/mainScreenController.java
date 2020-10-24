5d8cbc1a09483f5514a5ed174511056b10ec7f19
Marken <27118779+Marken2808@users.noreply.github.com>
2020-10-24 22:14:41 +0100

    Initial commit

---
+++ src/main/controllers/mainScreenController.java
@@ -0,0 +1,290 @@
+package main.controllers;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import main.Utils.Utils;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+import org.opencv.objdetect.Objdetect;
+import org.opencv.videoio.VideoCapture;
+
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class mainScreenController
+{
+    @FXML
+    private Button btnCam;
+
+    @FXML
+    private ImageView currentFrame;
+
+    @FXML
+    private CheckBox haarClassifier;
+    @FXML
+    private CheckBox lbpClassifier;
+
+    // a timer for acquiring the video stream
+    private ScheduledExecutorService timer;
+    // the OpenCV object that performs the video capture
+    private VideoCapture capture;
+    // a flag to change the button behavior
+    private boolean cameraActive;
+
+    // face cascade classifier
+    private CascadeClassifier faceCascade;
+    private int absoluteFaceSize;
+
+    /**
+     * Init the controller, at start time
+     */
+    public void init()
+    {
+        this.capture = new VideoCapture();
+        this.faceCascade = new CascadeClassifier();
+        this.absoluteFaceSize = 0;
+
+        // set a fixed width for the frame
+        currentFrame.setFitWidth(600);
+        // preserve image ratio
+        currentFrame.setPreserveRatio(true);
+    }
+
+    /**
+     * The action triggered by pushing the button on the GUI
+     */
+    @FXML
+    protected void startCamera()
+    {
+        if (!this.cameraActive)
+        {
+            // disable setting checkboxes
+            this.haarClassifier.setDisable(true);
+            this.lbpClassifier.setDisable(true);
+
+            // start the video capture
+            this.capture.open(0);
+
+            // is the video stream available?
+            if (this.capture.isOpened())
+            {
+                this.cameraActive = true;
+
+                // grab a frame every 33 ms (30 frames/sec)
+                Runnable frameGrabber = new Runnable() {
+
+                    @Override
+                    public void run()
+                    {
+                        // effectively grab and process a single frame
+                        Mat frame = grabFrame();
+                        // convert and show the frame
+                        Image imageToShow = Utils.mat2Image(frame);
+                        updateImageView(currentFrame, imageToShow);
+                    }
+                };
+
+                this.timer = Executors.newSingleThreadScheduledExecutor();
+                this.timer.scheduleAtFixedRate(frameGrabber, 0, 33, TimeUnit.MILLISECONDS);
+
+                // update the button content
+                this.btnCam.setText("Stop Camera");
+            }
+            else
+            {
+                // log the error
+                System.err.println("Failed to open the camera connection...");
+            }
+        }
+        else
+        {
+            // the camera is not active at this point
+            this.cameraActive = false;
+            // update again the button content
+            this.btnCam.setText("Start Camera");
+            // enable classifiers checkboxes
+            this.haarClassifier.setDisable(false);
+            this.lbpClassifier.setDisable(false);
+
+            // stop the timer
+            this.stopAcquisition();
+        }
+    }
+
+    /**
+     * Get a frame from the opened video stream (if any)
+     *
+     * @return the {@link Image} to show
+     */
+    private Mat grabFrame()
+    {
+        Mat frame = new Mat();
+
+        // check if the capture is open
+        if (this.capture.isOpened())
+        {
+            try
+            {
+                // read the current frame
+                this.capture.read(frame);
+
+                // if the frame is not empty, process it
+                if (!frame.empty())
+                {
+                    // face detection
+                    this.detectAndDisplay(frame);
+                }
+
+            }
+            catch (Exception e)
+            {
+                // log the (full) error
+                System.err.println("Exception during the image elaboration: " + e);
+            }
+        }
+
+        return frame;
+    }
+
+    /**
+     * Method for face detection and tracking
+     *
+     * @param frame
+     *            it looks for faces in this frame
+     */
+    private void detectAndDisplay(Mat frame)
+    {
+        MatOfRect faces = new MatOfRect();
+        Mat grayFrame = new Mat();
+
+        // convert the frame in gray scale
+        Imgproc.cvtColor(frame, grayFrame, Imgproc.COLOR_BGR2GRAY);
+        // equalize the frame histogram to improve the result
+        Imgproc.equalizeHist(grayFrame, grayFrame);
+
+        // compute minimum face size (20% of the frame height, in our case)
+        if (this.absoluteFaceSize == 0)
+        {
+            int height = grayFrame.rows();
+            if (Math.round(height * 0.2f) > 0)
+            {
+                this.absoluteFaceSize = Math.round(height * 0.2f);
+            }
+        }
+
+        // detect faces
+        this.faceCascade.detectMultiScale(grayFrame, faces, 1.1, 2, Objdetect.CASCADE_SCALE_IMAGE,
+                new Size(this.absoluteFaceSize, this.absoluteFaceSize), new Size());
+
+        // each rectangle in faces is a face: draw them!
+        Rect[] facesArray = faces.toArray();
+        for (int i = 0; i < facesArray.length; i++)
+            Imgproc.rectangle(frame, facesArray[i].tl(), facesArray[i].br(), new Scalar(0, 255, 0), 3);
+
+    }
+
+    /**
+     * The action triggered by selecting the Haar Classifier checkbox. It loads
+     * the trained set to be used for frontal face detection.
+     */
+
+    String resPath = "D:/UWE/Year_3/DSP/FinalProject/SchoolManagement/src/";
+    @FXML
+    protected void haarSelected(Event event)
+    {
+        // check whether the lpb checkbox is selected and deselect it
+        if (this.lbpClassifier.isSelected())
+            this.lbpClassifier.setSelected(false);
+
+        this.checkboxSelection(resPath + "resources/haarcascades/haarcascade_frontalface_alt2.xml");
+    }
+
+    /**
+     * The action triggered by selecting the LBP Classifier checkbox. It loads
+     * the trained set to be used for frontal face detection.
+     */
+    @FXML
+    protected void lbpSelected(Event event)
+    {
+        // check whether the haar checkbox is selected and deselect it
+        if (this.haarClassifier.isSelected())
+            this.haarClassifier.setSelected(false);
+
+        this.checkboxSelection("D:/UWE/Year_3/DSP/FinalProject/SchoolManagement/src/resources/lbpcascades/lbpcascade_frontalface.xml");
+    }
+
+    /**
+     * Method for loading a classifier trained set from disk
+     *
+     * @param classifierPath
+     *            the path on disk where a classifier trained set is located
+     */
+    private void checkboxSelection(String classifierPath)
+    {
+        // load the classifier(s)
+        this.faceCascade.load(classifierPath);
+
+        // now the video capture can start
+        this.btnCam.setDisable(false);
+    }
+
+    /**
+     * Stop the acquisition from the camera and release all the resources
+     */
+    private void stopAcquisition()
+    {
+        if (this.timer!=null && !this.timer.isShutdown())
+        {
+            try
+            {
+                // stop the timer
+                this.timer.shutdown();
+                this.timer.awaitTermination(33, TimeUnit.MILLISECONDS);
+            }
+            catch (InterruptedException e)
+            {
+                // log any exception
+                System.err.println("Exception in stopping the frame capture, trying to release the camera now... " + e);
+            }
+        }
+
+        if (this.capture.isOpened())
+        {
+            // release the camera
+            this.capture.release();
+        }
+    }
+
+    /**
+     * Update the {@link ImageView} in the JavaFX main thread
+     *
+     * @param view
+     *            the {@link ImageView} to update
+     * @param image
+     *            the {@link Image} to show
+     */
+    private void updateImageView(ImageView view, Image image)
+    {
+        Utils.onFXThread(view.imageProperty(), image);
+    }
+
+    /**
+     * On application close, stop the acquisition from the camera
+     */
+    public void setClosed()
+    {
+        this.stopAcquisition();
+    }
+
+}
